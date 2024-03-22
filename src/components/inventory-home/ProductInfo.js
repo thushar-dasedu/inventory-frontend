a35c3aff5642cbab89202c26c298ece1fad1f625
thushar-dasedu <thushar@dasedu.in>
2024-03-22 18:10:12 +0530

    commit on 22-03-2024

--- src/components/inventory-home/ProductInfo.js
+++ src/components/inventory-home/ProductInfo.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+ 
 const ProductInfo = () => {
   return (
     <div>
         <div className="product-image">
-            
+             <img src="/mobile.webp" alt="" />
         </div>
     </div>
   )
